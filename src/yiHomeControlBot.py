2e84fb152f61bb09d73398af3a81f719526e1bb3
luca giulianini <luca.giulianini2@studio.unibo.it>
2020-07-23 16:08:03 +0200

    No polling

--- src/yiHomeControlBot.py
+++ src/yiHomeControlBot.py
@@ -108,8 +108,8 @@ logger.info("Initialized Video-Analysis module")
 watch_directory = config["watchDirectory"]
 if (not os.path.isabs(watch_directory)):
     watch_directory = os.path.abspath(os.path.join(watch_directory, ".."))
-#watcher = osWatcher.Watcher(watch_directory, videoAnalysis, authChatIds, bot)
-watcher = pollingWatcher.Watcher(watch_directory, videoAnalysis, authChatIds, bot, config)
+watcher = osWatcher.Watcher(watch_directory, videoAnalysis, authChatIds, bot)
+#watcher = pollingWatcher.Watcher(watch_directory, videoAnalysis, authChatIds, bot, config)
 thread1 = threading.Thread(watcher.checkFolder())
 thread1.start()
 logger.info("File watchdog started")
