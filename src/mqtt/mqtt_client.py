c31257458586f1abd3478118e0513bc1f36836a8
Luca Giulianini <luca.giulianini2@studio.unibo.it>
2022-05-16 00:05:58 +0200

    Version 3.1.1, qos=0

--- src/mqtt/mqtt_client.py
+++ src/mqtt/mqtt_client.py
@@ -13,13 +13,14 @@ logger = logging.getLogger(os.path.basename(__file__))
 class MqttClient:
     def __init__(self, config):
         self.config = config
-        self.client = mqtt.Client(client_id="Bot-" + str(Random().randint(0, 1000)), protocol=mqtt.MQTTv5)
+        self.client = mqtt.Client(client_id="Bot-" + str(Random().randint(0, 1000)), protocol=mqtt.MQTTv311)
         self.init_mqtt_client()
 
     def init_mqtt_client(self):
         username = self.config["broker-mqtt"]["username"]
         password = self.config["broker-mqtt"]["password"]
         self.client.username_pw_set(username=username, password=password)
+        self.client.on_disconnect = self.on_disconnect
 
     def set_on_connect(self, function: Callable):
         self.client.on_connect = function
@@ -34,14 +35,13 @@ class MqttClient:
     def connect_and_start(self):
         server: str = self.config["broker-mqtt"]["ip"]
         if server:
-            self.client.connect(server, 1883, 60)
+            self.client.connect(server, 1883)
             self.client.loop_start()
-            self.client.on_disconnect = self.on_disconnect
             logger.info("MQTT client started")
         else:
             logger.warning("MQTT client not defined")
 
-    def publish(self, topic: str, payload: str, qos=1, retain=False,
+    def publish(self, topic: str, payload: str, qos=0, retain=False,
                 properties: paho.mqtt.properties.Properties = None):
         info = self.client.publish(topic, payload, qos=qos, retain=retain, properties=properties)
         logger.info(f"Publishing: {payload} on topic {topic} - published: {info.is_published()} "
